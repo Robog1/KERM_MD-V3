8f7cd1273fce342d180ce656844310d3aabd4838
Robog1 <testdulite2@outlook.com>
2024-09-30 08:22:53 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Kerm v3~v7W15cD3",
+  SESSION_ID: process.env.SESSION_ID || "Kerm v3~33K78x8u",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
